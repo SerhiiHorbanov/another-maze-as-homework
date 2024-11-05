a046ee5b82cdf093aa821650d1dba41a1f279477
Serhii <serikgorbanev@gmail.com>
2024-11-05 16:54:35 +0100

    Moved definition of plus operator for vectors to .cpp

--- Maze/Vector2i.cpp
+++ Maze/Vector2i.cpp
@@ -4,3 +4,8 @@ std::string Vector2i::ToString() const
 {
     return "{ " + std::to_string(x) + ", " + std::to_string(y) + " }";
 }
+
+Vector2i operator+(const Vector2i left, const Vector2i right)
+{
+    return Vector2i(left.x + right.x, left.y + right.y);
+}
--- Maze/Vector2i.h
+++ Maze/Vector2i.h
@@ -20,7 +20,4 @@ public:
     std::string ToString() const;
 };
 
-Vector2i operator+(const Vector2i left, const Vector2i right)
-{
-    return Vector2i(left.x + right.x, left.y + right.y);
-}
+Vector2i operator+(const Vector2i left, const Vector2i right);
