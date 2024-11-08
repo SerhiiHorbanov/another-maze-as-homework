d2e0a5bd0d7fdb0d9783bdc59e94a501dbeab082
Serhii <serikgorbanev@gmail.com>
2024-11-08 17:53:46 +0100

    Added jetpack

--- Maze/main.cpp
+++ Maze/main.cpp
@@ -9,23 +9,25 @@ const int wallsFrequencyPercents = 25;
 
 char map[height][width];
 
+enum TileType
+{
+    Wall = '#',
+    Floor = '.',
+    Finish = 'F',
+    Jetpack = 'J'
+};
 const char playerChar = '@';
-const char wallChar = '#';
-const char floorChar = '.';
-const char finishChar = 'F';
 
 int playerX = 0;
 int playerY = 0;
 
+bool playerHasJetpack = false;
+
+int directionX = 0;
+int directionY = 0;
+
 int dx = 0;
 int dy = 0;
-
-char GenerateTile()
-{
-    if (std::rand() % 100 < wallsFrequencyPercents)
-        return wallChar;
-    return floorChar;
-}
 
 std::pair<int, int> GetRandomMapPosition()
 {
@@ -35,6 +37,26 @@ std::pair<int, int> GetRandomMapPosition()
     return { x, y };
 }
 
+char GenerateTile()
+{
+    if (std::rand() % 100 < wallsFrequencyPercents)
+        return TileType::Wall;
+    return TileType::Floor;
+}
+void PlaceTileRandomlyOnFloor(TileType tile)
+{
+    std::pair<int, int> tilePosition;
+    char tileInPossiblePosition;
+
+    do
+    {
+        tilePosition = GetRandomMapPosition();
+        tileInPossiblePosition = map[tilePosition.second][tilePosition.first];
+    } 
+    while (tileInPossiblePosition != TileType::Floor);
+
+    map[tilePosition.second][tilePosition.first] = tile;
+}
 void GenerateMap()
 {
     srand(time(NULL));
@@ -46,9 +68,8 @@ void GenerateMap()
             map[y][x] = GenerateTile();
         }
     }
-
-    std::pair<int, int> finishPosition = GetRandomMapPosition();
-    map[finishPosition.first][finishPosition.second] = finishChar;
+    PlaceTileRandomlyOnFloor(TileType::Finish);
+    PlaceTileRandomlyOnFloor(TileType::Jetpack);
 }
 
 void RandomizePlayerPosition()
@@ -60,46 +81,74 @@ void RandomizePlayerPosition()
 
 bool IsWalkable(char tile)
 {
-    return tile != wallChar;
+    return tile != TileType::Wall && !(playerHasJetpack && tile == TileType::Jetpack);
 }
-
 bool isInMapBounds(int x, int y)
 {
     bool NotTooLow = x >= 0 && y >= 0;
     return x < width && y < height;
 }
-
-bool CanMove()
+bool CanMoveTo(int tileX, int tileY)
 {
-    int toX = playerX + dx;
-    int toY = playerY + dy;
-
-    if (isInMapBounds(toX, toY))
-        return IsWalkable(map[toY][toX]);
+    if (isInMapBounds(tileX, tileY))
+        return IsWalkable(map[tileY][tileX]);
     return false;
 }
 
+bool CanWalk()
+{
+    int toX = playerX + directionX;
+    int toY = playerY + directionY;
+    return CanMoveTo(toX, toY);
+}
+bool CanUseJetpack()
+{
+    if (!playerHasJetpack)
+        return false;
+
+    int toX = playerX + (directionX * 2);
+    int toY = playerY + (directionY * 2);
+    return CanMoveTo(toX, toY);
+}
+
+void UpdateMovingDeltas()
+{
+    if (CanWalk())
+    {
+        dx = directionX;
+        dy = directionY;
+        return;
+    }
+    if (CanUseJetpack())
+    {
+        dx = directionX * 2;
+        dy = directionY * 2;
+        playerHasJetpack = false;
+        return;
+    }
+    dx = 0;
+    dy = 0;
+}
 void Move()
 {
     playerX += dx;
     playerY += dy;
 }
 
+void CheckForPickups()
+{
+    if (map[playerY][playerX] == TileType::Jetpack)
+    {
+        playerHasJetpack = true;
+        map[playerY][playerX] = TileType::Floor;
+    }
+}
+
 void TryMove()
 {
-    if (CanMove())
-        Move();
-}
-
-bool GameEnded()
-{
-    return map[playerY][playerX] == finishChar;
-}
-
-void PlacePlayerInImage(std::string& image)
-{
-    const int index = playerY * (width + 1) + playerX;
-    image[index] = playerChar;
+    UpdateMovingDeltas();
+    Move();
+    CheckForPickups();
 }
 
 void PlaceTileInImage(std::string& image, int x, int y)
@@ -107,7 +156,11 @@ void PlaceTileInImage(std::string& image, int x, int y)
     const int charIndex = (y * (width + 1)) + x;
     image[charIndex] = map[y][x];
 }
-
+void PlacePlayerInImage(std::string& image)
+{
+    const int index = playerY * (width + 1) + playerX;
+    image[index] = playerChar;
+}
 std::string GetImage()
 {
     std::string result = std::string((width + 1) * height, '\n');
@@ -123,32 +176,32 @@ std::string GetImage()
     return result;
 }
 
+void ProcessInput(char input)
+{
+    directionY = 0;
+    directionX = 0;
+    switch (input)
+    {
+    case 'w':
+        directionY = -1;
+        break;
+    case 's':
+        directionY = 1;
+        break;
+    case 'a':
+        directionX = -1;
+        break;
+    case 'd':
+        directionX = 1;
+        break;
+    }
+}
+
 void Render()
 {
     std::system("cls");
     std::string image = GetImage();
     std::cout << image;
-}
-
-void ProcessInput(char input)
-{
-    dy = 0;
-    dx = 0;
-    switch (input)
-    {
-    case 'w':
-        dy = -1;
-        break;
-    case 's':
-        dy = 1;
-        break;
-    case 'a':
-        dx = -1;
-        break;
-    case 'd':
-        dx = 1;
-        break;
-    }
 }
 
 void Input()
@@ -161,6 +214,11 @@ void Input()
 void Update()
 {
     TryMove();
+}
+
+bool GameEnded()
+{
+    return map[playerY][playerX] == TileType::Finish;
 }
 
 int main()
